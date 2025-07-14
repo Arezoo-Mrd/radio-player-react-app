cd10caf3903992e1ae8ef7fb16b754fbb0b3beb5
ArezooMrd <moradi.arezoo1992@gmail.com>
2025-07-14 20:53:25 +0330

    fix: fix buttons

--- app/Home/components/PlayPauseBtn.tsx
+++ app/Home/components/PlayPauseBtn.tsx
@@ -14,7 +14,7 @@ const PlayPauseBtn = ({
     return (
         <button
             onClick={togglePlay}
-            className="md:w-16 md:h-16 w-8 h-8 sm:w-11 sm:h-11 bg-primary-base  cursor-pointer rounded-full flex items-center justify-center text-white shadow-lg hover:shadow-xl hover:scale-110 active:scale-95 transition-all duration-200 disabled:opacity-50 disabled:cursor-not-allowed"
+            className="md:w-16 md:h-16 w-11 h-11 bg-primary-base  cursor-pointer rounded-full flex items-center justify-center text-white shadow-lg hover:shadow-xl hover:scale-110 active:scale-95 transition-all duration-200 disabled:opacity-50 disabled:cursor-not-allowed"
         >
             {isLoading ? (
                 <div className="w-6 h-6 border-2 border-white border-t-transparent rounded-full animate-spin" />
